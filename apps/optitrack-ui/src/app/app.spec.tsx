b8659057ed076f193f3c69702984fa9b0987dd5e
Joshua Baran <joshua.baran@gmail.com>
2024-01-29 12:23:53 -0700

    fix test, add ci

--- apps/optitrack-ui/src/app/app.spec.tsx
+++ apps/optitrack-ui/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome to optitrack-ui/gi)).toBeTruthy();
+    expect(getByText(/optitrack/gi)).toBeTruthy();
   });
 });
