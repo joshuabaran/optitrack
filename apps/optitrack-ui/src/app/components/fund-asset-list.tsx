3f7d8d17166aaa625f2c86b4a2976de3022709a6
Joshua Baran <joshua.baran@gmail.com>
2024-01-30 00:40:25 -0700

    selected asset

--- apps/optitrack-ui/src/app/components/fund-asset-list.tsx
+++ apps/optitrack-ui/src/app/components/fund-asset-list.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import {
   Card,
   Text,
@@ -8,17 +9,23 @@ import { IAsset } from '../../../data/fund'
 
 export interface IFundAssetListProps {
   assets: IAsset[]
+  selected: number
+  setSelected: (index: number) => void
 }
 
 export const FundAssetList = (props: IFundAssetListProps) => {
-  const { assets } = props
+  const { assets, selected, setSelected } = props
+
+  useEffect(() => {
+    console.log(`FundAssetList: selected=${selected}`)
+  }, [selected])
 
   return (
     <Card>
       <Text>Asset List</Text>
       <Flex flexDirection="col" className="max-h-[700px] overflow-y-auto overflow-x-hidden p-10">
-        {assets.map((asset) => (
-          <Card className="m-2" decoration="left">
+        {assets.map((asset, idx) => (
+          <Card className="m-2 hover:cursor-pointer hover:border-orange-600" decoration="left" onClick={(evt) => {setSelected(idx)}}>
             <Text>{asset.name}</Text>
             <Text>{asset.symbol}</Text>
             <Text>{asset.sector}</Text>
--- apps/optitrack-ui/src/app/pages/dashboard.tsx
+++ apps/optitrack-ui/src/app/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Grid, Col } from '@tremor/react'
 import { useQuery } from 'react-query'
 
@@ -9,11 +10,15 @@ import {
   MarketTrendsList,
   Loading
 } from '../components/'
-
 import { getFund } from '../hooks'
 
 export const Dashboard = () => {
+  const [selectedAsset, setSelectedAsset] = useState(0)
   const { data: fundData, isLoading } = useQuery('overview', getFund)
+
+  useEffect(() => {
+    console.log('selectedAsset', { selectedAsset })
+  }, [selectedAsset])
 
   return (
     
@@ -23,7 +28,7 @@ export const Dashboard = () => {
             <Overview fund={fundData || null} />
           </Col>
           <Col numColSpan={1}>
-            <FundAssetList assets={fundData?.assets || []} />
+            <FundAssetList assets={fundData?.assets || []} selected={selectedAsset} setSelected={setSelectedAsset} />
           </Col>
           <Col numColSpan={1} numColSpanMd={2}>
             <AssetPriceChart />
